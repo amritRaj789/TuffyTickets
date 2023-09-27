f76619d555ac1424adaa26ab165fc158a29b2318
amritRaj789 <amritrazzz@gmail.com>
2023-09-26 22:56:06 -0700

    added the Occasions structure, function list to sol file and update occasion count to test file

--- contracts/tuffytickets.sol
+++ contracts/tuffytickets.sol
@@ -5,6 +5,20 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TuffyTickets is ERC721 {
     address public owner; // the developer in our case
+    uint256 public totalOccasions; // counter for number of Occasions
+
+    struct Occasion { // we cannot name a variable as Event in Solidity as it is a reserved keyword
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) occasions;
 
     constructor(
         string memory _name,
@@ -13,5 +27,27 @@ contract TuffyTickets is ERC721 {
             owner = msg.sender; // msg.sender is the adress of the person who is calling the constructor
     }
 
+    function list(
+        string memory _name, // event time
+        uint256 _cost, // ticket cost
+        uint256 _maxTickets, // max num to be sold for that event
+        string memory _date, // event date
+        string memory _time, // event time
+        string memory _location // event location
+        ) public{
+            totalOccasions += 1;
+
+            occasions[totalOccasions] = Occasion(
+                totalOccasions,
+                _name,
+                _cost,
+                _maxTickets,
+                _maxTickets,
+                _date,
+                _time,
+                _location
+            );
+
+    }
 }
 
--- test/tuffytickets.js
+++ test/tuffytickets.js
@@ -1,9 +1,18 @@
 const {expect} = require("chai");
 const { ethers } = require("hardhat");
-//const { ethers } = require("hardhat");
+
 
 const NAME = "TuffyTickets";
 const SYMBOL = "TT";
+
+// details for an event
+
+const OCCASION_NAME = "ETH CSUF Concert"
+const OCCASION_COST = ethers.utils.parseUnits('1', 'ether')
+const OCCASION_MAX_TICKETS = 169
+const OCCASION_DATE = "Apr 03"
+const OCCASION_TIME = "12:00PM PDT"
+const OCCASION_LOCATION = "Santa Ana, California"
 
 describe("TuffyTickets", () => {
     
@@ -24,14 +33,27 @@ describe("TuffyTickets", () => {
     })*/
 
     //shorter way is this
-    let TuffyTickets
+    let tuffyTickets;
     let deployer, buyer;
 
     beforeEach(async() => {
         // setup accounts
         [deployer, buyer] = await ethers.getSigners();
+
         const TuffyTickets = await ethers.getContractFactory("TuffyTickets");
         tuffyTickets = await TuffyTickets.deploy(NAME, SYMBOL)
+
+        const transaction = await tuffyTickets.connect(deployer).list(
+            OCCASION_NAME,
+            OCCASION_COST,
+            OCCASION_MAX_TICKETS,
+            OCCASION_DATE,
+            OCCASION_TIME,
+            OCCASION_LOCATION
+        ) // connect specifies the account with which we are doing transactions
+
+
+        await transaction.wait()
     })
 
     describe("Deployment", () => { 
@@ -44,7 +66,14 @@ describe("TuffyTickets", () => {
             expect(symbol).to.equal(SYMBOL)
         })
         it("Sets the owner", async() => {
-            expect(await tuffyTickets.owner()).to.equal(deployer.address)
+            expect(await tuffyTickets.owner()).to.equal(deployer.address) // people usually forget to add the address
+        })
+    })
+
+    describe("Occasions", () => {
+        it("Updates occasions count", async() => {
+            const totalOccasions = await tuffyTickets.totalOccasions()
+            expect(totalOccasions).to.be.equal(1)
         })
     })
 
