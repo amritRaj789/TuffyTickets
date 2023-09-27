80725da2d07ddd2d48a37800c282a00ff07a0a6e
amritRaj789 <amritrazzz@gmail.com>
2023-09-26 18:37:55 -0700

    added the 'set symbol' code to  test file

--- test/tuffytickets.js
+++ test/tuffytickets.js
@@ -1,13 +1,46 @@
 const {expect} = require("chai");
+//const { ethers } = require("hardhat");
+
+const NAME = "TuffyTickets";
+const SYMBOL = "TT";
 
 describe("TuffyTickets", () => {
     
-    describe("Deployment", () => {
-        it("Sets the name", async() => {
+    // this is longer way which adds redundancy
+    /*describe("Deployment", () => { 
+        it("Sets the name", async() => { // sets the name of the contract
             const TuffyTickets = await ethers.getContractFactory("TuffyTickets")
             let tuffyTickets = await TuffyTickets.deploy("TuffyTickets", "TT")
             let name = await tuffyTickets.name();
             expect(name).to.equal("TuffyTickets")
         })
+        it("Sets the symbol", async() => { // sets the name of the contract
+            const TuffyTickets = await ethers.getContractFactory("TuffyTickets")
+            let tuffyTickets = await TuffyTickets.deploy("TuffyTickets", "TT")
+            let symbol = await tuffyTickets.symbol();
+            expect(symbol).to.equal("TT")
+        })
+    })*/
+
+    //shorter way is this
+    let TuffyTickets
+
+    beforeEach(async() => {
+        // setup accounts
+        const TuffyTickets = await ethers.getContractFactory("TuffyTickets");
+        tuffyTickets = await TuffyTickets.deploy(NAME, SYMBOL)
     })
+
+    describe("Deployment", () => { 
+        it("Sets the name", async() => { // sets the name of the contract
+            let name = await tuffyTickets.name();
+            expect(name).to.equal(NAME)
+        })
+        it("Sets the symbol", async() => { // sets the name of the contract
+            let symbol = await tuffyTickets.symbol();
+            expect(symbol).to.equal(SYMBOL)
+        })
+    })
+
+
 })
