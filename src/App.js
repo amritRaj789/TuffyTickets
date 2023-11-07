a18cc88587f448a2d5d99ec268cd9557e79d8991
amritRaj789 <amritrazzz@gmail.com>
2023-11-07 12:58:57 -0800

    added index.css file contents

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 
 // Components
-import Navigation from './components/Navigation'
-import Sort from './components/Sort'
-import Card from './components/Card'
-import SeatChart from './components/SeatChart'
+//import Navigation from './components/Navigation'
+//import Sort from './components/Sort'
+//import Card from './components/Card'
+//import SeatChart from './components/SeatChart'
 
 // ABIs
-import TokenMaster from './abis/TuffyTickets.json'
+import TuffyTickets from './abis/TuffyTickets.json'
 
 // Config
 import config from './config.json'
