d7e422736b5b94ae713b9d1bb14897f4590e9eba
amritRaj789 <amritrazzz@gmail.com>
2023-11-14 18:22:15 -0800

    added front-end code to show events in card style

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { ethers } from 'ethers'
 
 // Components
 import Navigation from './components/Navigation'
-//import Sort from './components/Sort'
-//import Card from './components/Card'
+import Sort from './components/Sort'
+import Card from './components/Card'
 //import SeatChart from './components/SeatChart'
 
 // ABIs
@@ -16,13 +16,41 @@ import config from './config.json'
 function App() {
 
   const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [tuffyTickets, setTuffyTickets] = useState(null)
+  const [occasions, setOccasions] = useState([]) 
+  const [occasion, setOccasion] = useState({})
+  const [toggle, setToggle] = useState(false)
+
 
   const loadBlockchainData = async () => {
 
-    // Fetch Account
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
+    // // Fetch Account
+    // const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+    // const account = ethers.utils.getAddress(accounts[0])
+    // setAccount(account)
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    const address = config[network.chainId].TuffyTickets.address
+    const tuffyTickets = new ethers.Contract(address, TuffyTickets, provider)
+    setTuffyTickets(tuffyTickets)
+
+    const totalOccasions = await tuffyTickets.totalOccasions()
+    
+    const occasions = []
+
+    for(let i = 1; i <= totalOccasions; i++){
+      const occasion = await tuffyTickets.getOccasion(i)
+      occasions.push(occasion)
+    }
+
+    setOccasions(occasions)
+
+    console.log(occasions)
+    console.log(tuffyTickets.address)
 
     // Refresh Account
     window.ethereum.on('accountsChanged', async () => {
@@ -43,8 +71,24 @@ function App() {
         <Navigation account={account} setAccount={setAccount} />
         <h2 className='header__title'><strong>Event Tickets</strong></h2>
       </header>
-        <h1>Hello, CPSC 597</h1>
-        <p>{account}</p>
+
+      <Sort />
+
+      <div className='cards'>
+        {occasions.map((occasion, index) => (
+          <Card
+            occasion = {occasion}
+            id = {index+1}
+            tuffyTickets = {tuffyTickets}
+            provider = {provider}
+            account = {account}
+            toggle = {toggle}
+            setToggle = {setToggle}
+            setOccasion = {setOccasion}
+            key = {index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
